b5d3f6363f3b9e082352113b4e40d22adaa2ab2c
Christopher-MT <chris56mt@gmail.com>
2024-12-06 11:40:01 -0500

    bucle loop

--- let.js
+++ let.js
@@ -1,76 +1,90 @@
-// let five = 10;
-// console.log(five +  five);
+// // let five = 10;
+// // console.log(five +  five);
 
-let johan = 8;
-let weimar = ""
+// let johan = 8;
+// let weimar = ""
 
-for (gay = 0; gay < johan; gay++){
-    for ( carepa = 0; carepa < johan; carepa++){
+// for (gay = 0; gay < johan; gay++){
+//     for ( carepa = 0; carepa < johan; carepa++){
 
-        if ((carepa + gay)% 2 == 0){
-            weimar += " ";
-        }else {
-            weimar +="$";
-        }
-    }
-    weimar += "\n";
-}
-console.log(weimar)
-
-// // CON BUCLE
-// let etiquetas = [];
-// let repetir = 5;
-// for(let i = 0; i < repetir; i ++) {
-//     etiquetas.push(`Usuario ${i + 1}`)
-// };
-
-// console.log(etiquetas);
-
-// CON FUNCIÓN
-function repeti(veces, callback) {
-    for (let i = 0; i < veces; i++) {
-        callback(i);
-    }
-}
-
-let etiqueta = [];
-    repeti (5, i => {
-    etiqueta.push(`Usuario ${i + 1}`)
-})
-
-console.log(etiqueta)
-
-
-// function repetir(veces, callback) {
-//     for (let i = 0; i < veces; i++) {
-//       callback(i);
+//         if ((carepa + gay)% 2 == 0){
+//             weimar += " ";
+//         }else {
+//             weimar +="$";
+//         }
 //     }
-//   }
+//     weimar += "\n";
+// }
+// console.log(weimar)
+
+// // // CON BUCLE
+// // let etiquetas = [];
+// // let repetir = 5;
+// // for(let i = 0; i < repetir; i ++) {
+// //     etiquetas.push(`Usuario ${i + 1}`)
+// // };
+
+// // console.log(etiquetas);
+
+// // CON FUNCIÓN
+// function repeti(veces, callback) {
+//     for (let i = 0; i < veces; i++) {
+//         callback(i);
+//     }
+// }
+
+// let etiqueta = [];
+//     repeti (5, i => {
+//     etiqueta.push(`Usuario ${i + 1}`)
+// })
+
+// console.log(etiqueta)
+
+
+// // function repetir(veces, callback) {
+// //     for (let i = 0; i < veces; i++) {
+// //       callback(i);
+// //     }
+// //   }
   
-//   let etiqueta = [];
-//   repetir(5, i => {
-//     etiqueta.push(`Usuario ${i + 1}`);
-//   });
+// //   let etiqueta = [];
+// //   repetir(5, i => {
+// //     etiqueta.push(`Usuario ${i + 1}`);
+// //   });
   
-//   console.log(etiqueta);
+// //   console.log(etiqueta);
 
 
-let numero = 0;
+// let numero = 0;
 
-while (numero <= 18) {
-    console.log(numero)
-    numero = numero +2;
+// while (numero <= 18) {
+//     console.log(numero)
+//     numero = numero +2;
+// }
+// console.log(numero)
+
+
+// for (let actual = 20; ;actual = actual + 1) {
+//         if(actual % 7 == 0){
+//             console.log(actual);
+//             break
+//         }
+// }
+
+// let persona = {a: 10, b: 50, c: 70}
+
+// console.log(persona);
+
+
+function loop(start, test, update, body) {
+    while (test(start)) {
+        body(start);
+        start = update(start)
+    }
 }
-console.log(numero)
-
-
-for (let actual = 20; ;actual = actual + 1) {
-        if(actual % 7 == 0){
-            console.log(actual);
-            break
-        }
-}
-
-let persona = {a: 10, b: 50, c: 70}
-
-console.log(persona);
+loop(
+3,
+n => n > 0,
+n => n - 1,
+console.log
+);
