3a39e0138a5fcc0fbd670daf6b0b6e997f692974
Christopher-MT <chris56mt@gmail.com>
2024-12-06 18:10:00 -0500

    avance del ejecicio Dominant_Direction

---
+++ capitulo5_ejem/Ejercicios/DominantDirection.js
@@ -0,0 +1,15 @@
+function getDominantwritedirection(text) {
+    const LTR = "ltr";
+    const RTL = "rtl";
+    const TTB = "ttb";
+}
+
+const LTR_Ranges = [
+    [0x0041, 0x005A],
+    [0x0061, 0x007A],
+    [0x0030, 0x0039]
+]
+
+const RTL_Ranges = [
+    
+]
